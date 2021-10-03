6dbb2b7926fe37080f655cfa5902549e523dd0e7
bekkolya <62704279+bekkolya@users.noreply.github.com>
2021-10-03 13:45:59 +0300

    Add files via upload

---
+++ Clone.bat
@@ -0,0 +1,2 @@
+rd /s /q "C:\Users\bekk-\source\repos\Project\Clone"
+git clone C:\Users\bekk-\source\repos\Project\Test\ C:\Users\bekk-\source\repos\Project\Clone
---
+++ Test.bat
@@ -0,0 +1,3 @@
+cd C:\Users\bekk-\source\repos\Project\Clone\MyCalcLibTests\bin\Debug
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" MyCalcLibTests.dll
+PAUSE
