9e072ea355fa15dbbba0b0982668dd8a462a2255
bekkolya <62704279+bekkolya@users.noreply.github.com>
2021-10-03 13:45:18 +0300

    Delete Test.bat

--- Test.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\bekk-\source\repos\MyCalcLib\MyCalcLibTests\bin\Debug
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" MyCalcLibTests.dll
-PAUSE
